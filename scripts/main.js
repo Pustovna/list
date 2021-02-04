37393f42388ea582fb9f9c3f7fce875b7106a570
Anastasiya <pustoe0@gmail.com>
2021-02-04 19:54:47 +0300

    new functional

--- scripts/main.js
+++ scripts/main.js
@@ -1,22 +1,24 @@
 const template = document.querySelector('#task-template').content;
 const todoList = document.querySelector('.todo-list');
-const form = document.querySelector('.add-form')
-
-//отправка данных
-const postItems  = async (url, data) => {
-    let res = await fetch(url, {
-        method: 'POST',
-        body: JSON.stringify(data)
-    });
-    return await res.text();
-}
+const form = document.querySelector('.add-form');
+const modal = document.querySelector('.modal')
+const buttons = modal.querySelectorAll('.modal-button');
+const deleteButton = todoList.querySelectorAll('.todo-list-input')
+let checkedItem;
 
 const addItem = (text) => {
     const newItem = template.cloneNode(true);
     const newItemText = newItem.querySelector('span');
     newItemText.textContent = text;
     todoList.appendChild(newItem);
-}
+
+    const button = todoList.lastElementChild.querySelector('.todo-list-input');
+    showModal(button);
+};
+
+const getItems = () => {
+    return fetch('http://localhost:63342/list/data.json').then(response => response.json());
+};
 
 form.addEventListener('submit', (evt) => {
     evt.preventDefault();
@@ -24,19 +26,41 @@ form.addEventListener('submit', (evt) => {
     const text = inputText.value;
     addItem(text);
     inputText.value = '';
-
-    postItems('http://localhost:63342/list/data.json', text).then(r => {
-        console.log(r);
-    });
 });
 
-// получение данных из json
-const getItems = () => {
-    return fetch('http://localhost:63342/list/data.json').then(response => response.json());
-}
+const showModal = (button) => {
+    button.addEventListener('click', (e) => {
+        modal.style.display = "flex";
+        checkedItem = e.target;
+    })
+};
 
-
-
-window.addEventListener('DomContentLoaded', () => {
-    getItems();
+deleteButton.forEach(button => {
+    showModal(button);
 });
+
+buttons.forEach(button => {
+    button.addEventListener('click', () => {
+        if (button.dataset.button === 'yes') {
+            getItems().then((data) => {
+                const itemList = checkedItem.closest('.todo-list-item')
+                for (let item in data) {
+                    if (data[item] == true) {
+                        itemList.remove();
+                        modal.style.display = "none";
+                    }
+                }
+            })
+                .catch(() => {
+                    const text = modal.querySelector('span');
+                    text.textContent = 'Error';
+                });
+        } else {
+            modal.style.display = "none";
+        }
+    })
+})
+
+
+
+
